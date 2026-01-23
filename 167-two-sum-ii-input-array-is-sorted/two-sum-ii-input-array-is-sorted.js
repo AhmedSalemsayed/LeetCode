bfb50c419085076a0fdbd09d0ad584e70a473d08
Ahmed Salem Sayed <137791161+AhmedSalemsayed@users.noreply.github.com>
2026-01-23 14:59:58 +0200

    Time: 8 ms (8.71%) | Memory: 55.6 MB (54.95%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+     for (let i = 0; i < numbers.length; i++) {
+            let l = i + 1,
+                r = numbers.length - 1;
+            let tmp = target - numbers[i];
+            while (l <= r) {
+                let mid = l + Math.floor((r - l) / 2);
+                if (numbers[mid] === tmp) {
+                    return [i + 1, mid + 1];
+                } else if (numbers[mid] < tmp) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return [];
+}
