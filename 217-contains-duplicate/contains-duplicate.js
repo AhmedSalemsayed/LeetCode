2d29ffcb78c672025908281d3ea87b20459087fb
Ahmed Salem Sayed <137791161+AhmedSalemsayed@users.noreply.github.com>
2026-01-23 21:22:29 +0200

    Time: 9 ms (97.69%) | Memory: 74.4 MB (40.91%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numSet = new Set();
+  for (const num of nums) {
+    if (numSet.has(num)) {
+      return true;
+    } else {
+      numSet.add(num);
+    }
+  }
+  return false;
+};
