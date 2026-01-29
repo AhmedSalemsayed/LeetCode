5e839853856a2245f049d67fdcb4ac8f801d8356
Ahmed Salem Sayed <137791161+AhmedSalemsayed@users.noreply.github.com>
2026-01-29 13:43:15 +0200

    Time: 6 ms (45.62%) | Memory: 56.5 MB (26.40%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let obj = {};
+
+        for (let i = 0; i < nums.length; i++){
+          obj[nums[i]] = i
+        }
+
+        for (let i = 0; i < nums.length; i++){
+          let complement = target - nums[i];
+          
+          if(obj[complement] !== undefined && obj[complement] !== i){
+            return [i , obj[complement]]
+          }
+        }
+        return [];
+};
